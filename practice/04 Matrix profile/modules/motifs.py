72ab239a4d53947ac7aa3905cfd086afdf6c78e4
Kerbabyk <40473665+Kerbabyk@users.noreply.github.com>
2024-11-04 16:42:13 +0500

    Update motifs.py

--- practice/04 Matrix profile/modules/motifs.py
+++ practice/04 Matrix profile/modules/motifs.py
@@ -27,10 +27,7 @@ def top_k_motifs(matrix_profile: dict, top_k: int = 3) -> dict:
 
     sorted_indices = np.argsort(mp)
 
-    filtered_indices = []
-    for idx in sorted_indices:
-        if all(abs(idx - other_idx) > excl_zone for other_idx in filtered_indices):
-            filtered_indices.append(idx)
+    filtered_indices = apply_exclusion_zone(sorted_indices, excl_zone)
 
     for idx in filtered_indices[:top_k]:
         left_idx = idx
